ac5a7a48a21d90b25cf44782536b8a610cca6f5d
matt-weiner <mweiner@umass.edu>
2023-10-18 13:24:40 -0400

    created basic patient model + dashboard

--- patientTracker/patientTracker/urls.py
+++ patientTracker/patientTracker/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from patient.views import doctor_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('dashboard',doctor_dashboard,name="dashboard")
 ]
