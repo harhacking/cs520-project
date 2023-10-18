18b4b6a0c848d9483c2d0b8360a4095686cb50ba
matt-weiner <mweiner@umass.edu>
2023-10-18 13:36:52 -0400

    created doctor model

--- patientTracker/patient/models.py
+++ patientTracker/patient/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from doctor.models import Doctor
 # Create your models here.
 
 class Patient(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor,on_delete=models.CASCADE, related_name="patients")
     name = models.CharField(max_length=100)
     medical_history = models.TextField()
     medications = models.TextField()
