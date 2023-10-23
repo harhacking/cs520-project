e3a8fa5db26a5414ca7b158e2b4089e37be25e7a
matt-weiner <mweiner@umass.edu>
2023-10-23 14:41:47 -0400

    cleanup

--- patientTracker/patientTracker/urls.py
+++ patientTracker/patientTracker/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from patient.views import doctor_dashboard
+from django.urls import path
 from doctor.views import doctor_appointments,register_doctor
 from CustomUser.views import auth_user
 
