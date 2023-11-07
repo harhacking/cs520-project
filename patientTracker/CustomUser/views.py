d6cd0d9088e9e34cc2c1dbb1a101c1636950abf8
matt-weiner <mweiner@umass.edu>
2023-11-06 20:56:59 -0500

    update auth function

--- patientTracker/CustomUser/views.py
+++ patientTracker/CustomUser/views.py
@@ -4,13 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseForbidden, JsonResponse, HttpResponseBadRequest
 from doctor.models import Doctor
+from django.forms.models import model_to_dict
 from patient.models import Patient
 
 
 
 @csrf_exempt
 def auth_user(request):
-    
     try:
         data = json.loads(request.body)
         username = data['username']
@@ -24,25 +24,10 @@ def auth_user(request):
         return_obj = None
         if(user.is_doctor):
             doctor = Doctor.objects.get(user=user)
-            id = doctor.id
-            return_obj = {
-            'is_doctor': user.is_doctor,
-            'doctor_id': id,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email,
-            'specialization': doctor.specialization
-        }
+            return_obj = model_to_dict(doctor)
         else:
             patient = Patient.objects.get(user=user)
-            id = patient.id
-            return_obj = {
-            'is_doctor': user.is_doctor,
-            'patient_id': id,
-            'first_name': user.first_name,
-            'last_name': user.last_name,
-            'email': user.email
-        }
+            return_obj = model_to_dict(patient)
         login(request,user)
         return JsonResponse(return_obj,status=200)
     else:       
